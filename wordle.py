c3c28f97740f224c70fab78a6e74facfc13e8a69
sologuboved <sologuboved@gmail.com>
2022-02-22 19:11:20 +0300

    wip

--- wordle.py
+++ wordle.py
@@ -1,23 +1,22 @@
 def get_verdict(candidate, word):
     candidate = candidate.lower()
-    if len(candidate) != len(word):
-        return "Wrong lenth"
     masked = list('_' * len(word))
     incl = list()
     excl = set()
-    for indx in range(len(candidate)):
-        letter = candidate[indx]
-        if word[indx] == letter:
-            masked[indx] = letter
-            continue
-        if letter in word:
-            incl.append(letter)
-        else:
-            excl.add(letter)
+    if len(candidate) == len(word):
+        for indx in range(len(candidate)):
+            letter = candidate[indx]
+            if word[indx] == letter:
+                masked[indx] = letter
+                continue
+            if letter in word:
+                incl.append(letter)
+            else:
+                excl.add(letter)
+    else:
+        print("Wrong lenth")
     return ''.join(masked), incl, excl
 
 
 if __name__ == '__main__':
     print(get_verdict('skate', 'place'))
-
-
