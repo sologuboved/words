401962cc4e7fb156f8101837369f03a8de5090da
sologuboved <sologuboved@gmail.com>
2022-04-14 22:18:47 +0300

    now registers wrong positions

--- picker.py
+++ picker.py
@@ -1,12 +1,12 @@
 from itertools import permutations
 
 
-def pick(initial_masked, incl, excl, sample_size=float('inf')):
+def pick(initial_masked, wrong_placed, incl, excl, sample_size=float('inf')):
     suitables = set()
     incl += ['_' for _ in range(initial_masked.count('_') - len(incl))]
     for perm in permutations(incl, initial_masked.count('_')):
         masked_candidate = get_masked_candidate(initial_masked, perm)
-        suitables |= find_suitables(masked_candidate, excl, sample_size)
+        suitables |= find_suitables(masked_candidate, wrong_placed, excl, sample_size)
         if len(suitables) >= sample_size:
             break
     print(f"Found {len(suitables)}")
@@ -26,28 +26,32 @@ def get_masked_candidate(initial_masked, perm):
     return masked_candidate
 
 
-def find_suitables(masked, excl, sample_size):
+def find_suitables(masked, wrong_placed, excl, sample_size):
     suitables = set()
     with open('english.txt') as handler:
         for word in handler:
             word = word[:-1]
-            if is_suitable(masked, excl, word):
+            if is_suitable(masked, wrong_placed, excl, word):
                 suitables.add(word)
                 if len(suitables) == sample_size:
                     break
     return suitables
 
 
-def is_suitable(masked, excl, word):
+def is_suitable(masked, wrong_placed, excl, word):
     if len(word) != len(masked):
         return False
     if set(word) & excl:
         return False
-    for item in zip(masked, word):
-        if len(set(item) - {'_'}) > 1:
+    for indx in range(len(masked)):
+        char = word[indx]
+        for item in wrong_placed:
+            if char == item[indx]:
+                return False
+        if masked[indx] not in ('_', char):
             return False
     return True
 
 
 if __name__ == '__main__':
-    pick('ch___', [], {'p', 'l', 'a', 'e', 'o', 'r', 'd'}, 30)
+    pick('___ce', ['__i__'], ['i'], {'p', 'l', 'a', 'v', 'o'}, 30)
